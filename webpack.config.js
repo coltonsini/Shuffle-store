4fc91e18f9cc178e0f8547d265a71506ebfcf0ed
Juan José Ordoñez Villalobos <49448784+coltonsini@users.noreply.github.com>
2025-01-18 21:46:04 -0500

    Update webpack.config.js
    Added some extra configurations for the webpack build element.

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/app.js',
@@ -12,7 +13,18 @@ module.exports = {
         test: /\.scss$/,
         use: ['style-loader', 'css-loader', 'sass-loader'],
       },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|avif)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
+  plugins: [
+    new CleanWebpackPlugin(),
+  ],
   mode: 'development',
 };
